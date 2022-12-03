fda96b930371c36337b75020b12c9735aca986d8
Cade Luinenburg <videogamefpslover@gmail.com>
2022-12-03 14:40:18 -0500

    cleaning and updating code

--- D3/main.py
+++ D3/main.py
@@ -12,36 +12,35 @@ def getValue(item):
     if (item == newItem): value += 26
     return value
 
+def findBadge(sacks):
+    for badge1 in sacks[0]:
+        for badge2 in sacks[1]:
+            for badge3 in sacks[2]:
+                if (badge3 == badge2 == badge1):
+                    return getValue(badge1)
+
 def partOne():
     score = 0
     for line in file.readlines():
-        dupe = findDupe(line[0:len(line)//2], line[len(line)//2:len(line)])
+        dupe = findDupe(line[0:len(line)//2], line[len(line)//2:])
         score += getValue(dupe)
     return score
 
-
+def help():
+    line = "jVTBgVbgJQVrTLRRsLvRzWcZvnDs"
+    print(findDupe(line[0:len(line)//2], line[len(line)//2:len(line)]))
 
 def partTwo():
     score = 0
-    scoreToGive = 0
-    loops = 0
     sacks = []
-    for line in file.readlines():
+    for line, index in enumerate(file.readlines()):
         sacks.append(line[0:len(line)-1])
-        loops+=1
-        if (loops%3 == 0):
-            print(sacks)
-            for badge1 in sacks[0]:
-                for badge2 in sacks[1]:
-                    for badge3 in sacks[2]:
-                        if (badge3 == badge2 == badge1):
-                            scoreToGive = getValue(badge1)
-            loops = 0
+        if ((index+1)%3 == 0):
+            score += findBadge(sacks)
             sacks = []
-            score += scoreToGive
     return score
 
 
-print(partTwo())
+print(help())
 
 file.close()
