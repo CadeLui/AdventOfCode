3adad20f66290df207869f38af8c05455aaf00e3
Cade Luinenburg <videogamefpslover@gmail.com>
2022-12-07 16:26:29 -0500

    am i getting closer or further?

--- D7/main.py
+++ D7/main.py
@@ -48,26 +48,43 @@ class d:
                 result += (" " * (indent+1)) + "> " + obj.get_name() + ":" + str(obj.get_size()) + ":file" + "\n"
         return result[:-1]
 
+    def get_directories(self):
+        directories = []
+        for obj in self.directory:
+            if isinstance(obj, d):
+                directories.append(obj)
+                directories.extend(obj.get_directories())
+        return directories
+
 root_directory = d("/")
 
 def parseLS(log: list[str], currentLine: int):
+    # gets the current line, tests if it is an "ls" command
     line = log[currentLine][:-1]
     if line.split(" ")[1] != "ls":
-        print("notLs")
         return
 
+    # Gets the line before the "ls" command, grabs the directory we are currently inside
     prevLine = log[currentLine-1][:-1]
     directory = root_directory.get_sub_directory(prevLine.split(" ")[-1])
 
+    # for each line after "ls"
     for iterator in range(currentLine+1, len(log)):
+        # grab the line currently being read
         currentLine = log[iterator][:-1]
+
+        # if line is a command, escape
         if currentLine[0] == "$":
-            print("funcLeave")
             return
+
         splitLine = currentLine.split(" ")
+
+        # if line starts with number, add as a file
         if splitLine[0].isnumeric(): 
             directory.add_file(splitLine[1], int(splitLine[0]))
-        elif splitLine[0] == "dir":
+        
+        # if line starts with "dir", add as directory
+        if splitLine[0] == "dir":
             directory.add_directory(splitLine[1])
 
 file = open("input")
@@ -76,13 +93,17 @@ lines = file.readlines()
 
 for lineNum, line in enumerate(lines):
     parseLS(lines, lineNum)
+    #print(root_directory.get_inside() + "\n")
 
 print(root_directory.get_inside())
 
-total = 0
-for obj in root_directory.get_directory():
-    if isinstance(obj, d):
+def get_size_less_than_100000(directories: list, total: int):
+    for obj in directories:
+        print(obj.get_name() + ":" + str(obj.get_size()), end=":")
         if obj.get_size() <= 100000:
+            print("ADDED")
             total += obj.get_size()
+        else: print("SKIPPED")
+    return total
 
-print(total)
+print(get_size_less_than_100000(root_directory.get_directories(), 0))
