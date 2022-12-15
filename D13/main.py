91040297ffb8a0af0cbf53c3d3ca82d5372b0845
Cade Luinenburg <videogamefpslover@gmail.com>
2022-12-15 14:06:45 -0500

    making attempts (by stealing code)

--- D13/main.py
+++ D13/main.py
@@ -1,3 +1,6 @@
+from functools import cmp_to_key
+from math import prod
+
 def getPairs(lines: list[str]) -> list():
     pairs = []
     pair = []
@@ -7,6 +10,7 @@ def getPairs(lines: list[str]) -> list():
             pair = []
             continue
         pair.append(eval(line))
+    pairs.append(pair)
     return pairs
 
 def compareInt(left: int, right: int) -> int:
@@ -34,21 +38,39 @@ def compare(left: list, right: list) -> int:
     if iterator == len(left) and iterator == len(right):
         return 0            
     elif(iterator == len(left)):
-        return -1
-    else:
         return 1
+    else:
+        return -1
+
+def displayPairs(pairs: list) -> None:
+    for pair in pairs:
+        print(pair[0], "+", pair[1])
+
+def reorderPairs(pairs: list) -> list:
+    orderedPairs = []
+    for index, pair in enumerate(pairs):
+        comp = compare(pair[0], pair[1])
+        if comp == -1:
+            orderedPairs.append([pair[1], pair[0]])
+        else:
+            orderedPairs.append(pair)
+    return orderedPairs
+
 
 def partOne(lines: list[str]) -> None:
     pairs = getPairs(lines)
-    sum = 0
+    num = 0
     for index, pair in enumerate(pairs):
         comp = compare(pair[0], pair[1])
         if comp == 1 or comp == 0:
-            sum += index+1
-    print(sum)
+            num += index+1
+    print(num)
 
 def partTwo(lines: list[str]) -> None:
-    pass
+    pairs = getPairs(lines)
+    pairs.append([2]); pairs.append([6])
+    pairs = sorted(pairs, key=cmp_to_key(compare))
+    print(prod([idx+1 for idx,v in enumerate(pairs) if v==[[2]] or v == [[6]]]))
 
 def main(filename: str) -> None:
     lines = [l.strip() for l in open(filename).readlines()]
